4f98e77b15955e0d131f29b9cbda5ce4597785f9
chachar505 <fernandafajardo35@gmail.com>
2026-08-26 12:21:50 -0400

    primer boceto e ideas

---
+++ SteamDeTemu/js/videojuegos.js
@@ -0,0 +1,43 @@
+const videojuegos = [
+
+    {
+        id: 1,
+        nombre: "The Legend of Zelda",
+        genero: "Aventura",
+        plataforma: "Nintendo Switch",
+        anio: 2023,
+        valoracion: 9.5,
+        imagen: "img/juegos/zelda.jpg"
+    },
+
+    {
+        id: 2,
+        nombre: "Minecraft",
+        genero: "Supervivencia",
+        plataforma: "PC",
+        anio: 2011,
+        valoracion: 9.0,
+        imagen: "img/juegos/minecraft.jpg"
+    },
+
+    {
+        id: 3,
+        nombre: "God of War",
+        genero: "Acción",
+        plataforma: "PC",
+        anio: 2018,
+        valoracion: 9.2,
+        imagen: "img/juegos/god-of-war.jpg"
+    },
+
+    {
+        id: 4,
+        nombre: "Elden Ring",
+        genero: "RPG",
+        plataforma: "PC",
+        anio: 2022,
+        valoracion: 9.5,
+        imagen: "img/juegos/elden-ring.jpg"
+    }
+
+];
